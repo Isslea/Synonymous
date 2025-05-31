f582ca0edd357d32037089adc0c4a50615a10ab2
Isslea <91389716+Isslea@users.noreply.github.com>
2025-05-31 14:57:58 +0200

    add constants and ad hoc split polish synomuous

--- src/Synonymous/__init__.py
+++ src/Synonymous/__init__.py
@@ -6,11 +6,19 @@ import logging
 import re
 logging.basicConfig(level=logging.DEBUG, filename='anki_debug.log', filemode='w')
 
-
+#Card type
 DOUBLE = "1. Podwójny (wpisywanie odpowiedzi)"
 TYPE_ENGLISH = "2. Pojedynczy (wpisywanie angielski)"
 READ_POLISH = "3. Podstawowy (odczytywanie polski)"
 
+#Fields
+FOREIGN_FIELD = "Foreign/Content"
+POLISH_FIELD = "Polish/MultiLuka"
+AUDIO_FIELD = "Audio"
+IMAGE_FIELD = "Image"
+PART_OF_SPEECH_FIELD = "PartOfSpeech"
+EXTRA_FIELD = "Extra"
+DIALECT_FIELD = "Dialect"
 
 def move_queue_to_top(note_id, browser: Browser):
     db = browser.mw.col.db
@@ -81,7 +89,7 @@ def add_english_note(selected_notes, deck_id, browser: Browser):
             values = {val for val in values if val.strip()}
 
         delimiter = ', '
-        if field_name in ["Extra", "Image", "Audio"]:
+        if field_name in [EXTRA_FIELD, IMAGE_FIELD, AUDIO_FIELD]:
             delimiter = "<br>"
         new_note[field_name] = delimiter.join(sorted(values))
 
@@ -89,13 +97,13 @@ def add_english_note(selected_notes, deck_id, browser: Browser):
             count_words = len(values)
 
     if count_words > 1:
-        new_note["Polish/MultiLuka"] += f' [{count_words}]'
-    if len(combined_fields['Dialect']) > 0:
-        for dial in combined_fields['Dialect']:
+        new_note[POLISH_FIELD] += f' [{count_words}]'
+    if len(combined_fields[DIALECT_FIELD]) > 0:
+        for dial in combined_fields[DIALECT_FIELD]:
             for note_id in selected_notes:
                 note = browser.mw.col.get_note(note_id)
-                if dial in note['Dialect']:
-                    new_note['Extra'] += f'<br>{dial} - {note["Polish/MultiLuka"]}'
+                if dial in note[DIALECT_FIELD]:
+                    new_note[EXTRA_FIELD] += f'<br>{dial} - {note[POLISH_FIELD]}'
 
     browser.mw.col.add_note(new_note, deck_id)
     move_queue_to_top(new_note.id, browser)
@@ -115,18 +123,18 @@ def combine_english_synonymous(browser: Browser):
     showInfo(f"Done")
 
 def add_word_to_existing_note(browser, note, word):
-    note["Foreign/Content"] += f", {word}"
+    note[FOREIGN_FIELD] += f", {word}"
 
-    old_polish_text = note["Polish/MultiLuka"]
+    old_polish_text = note[POLISH_FIELD]
     match = re.search(r'\[(\d+)\]', old_polish_text)
     if match:
         number = int(match.group(1))
         new_number = number + 1
         old = match.group(0)
         new = f'[{new_number}]'
-        note["Polish/MultiLuka"] = old_polish_text.replace(old, new, 1)
+        note[POLISH_FIELD] = old_polish_text.replace(old, new, 1)
     else:
-        note["Polish/MultiLuka"] += " [2]"
+        note[POLISH_FIELD] += " [2]"
 
     note.flush()
     mw.col.reset()
@@ -136,11 +144,11 @@ def add_new_polish_note_for_synomymous(browser, note, word):
     polish_model = browser.mw.col.models.by_name(READ_POLISH)
     new_polish_note = browser.mw.col.new_note(polish_model)
 
-    new_polish_note["Foreign/Content"] = word
-    new_polish_note["Polish/MultiLuka"] = re.sub(r'\[\d+\]', '', note["Polish/MultiLuka"])
-    new_polish_note["Image"] = note["Image"]
-    new_polish_note["PartOfSpeech"] = note["PartOfSpeech"]
-    new_polish_note["Extra"] = note["Extra"]
+    new_polish_note[FOREIGN_FIELD] = word
+    new_polish_note[POLISH_FIELD] = re.sub(r'\[\d+\]', '', note[POLISH_FIELD])
+    new_polish_note[IMAGE_FIELD] = note[IMAGE_FIELD]
+    new_polish_note[PART_OF_SPEECH_FIELD] = note[PART_OF_SPEECH_FIELD]
+    new_polish_note[EXTRA_FIELD] = note[EXTRA_FIELD]
 
     deck_id = note.cards()[0].did
     browser.mw.col.add_note(new_polish_note, deck_id)
@@ -177,19 +185,67 @@ def add_english_synonym(browser: Browser):
 
     showInfo(f"Added synonym {word}")
 
+def split_polish_synonymous(browser: Browser):
+    selected = browser.selectedNotes()
+    if not selected:
+        showInfo("Please select one note.")
+        return
+    if len(selected) > 1:
+        showInfo("Please select only one note.")
+        return
+
+    nid = selected[0]
+    note = mw.col.get_note(nid)
+
+    splitted_note = note[POLISH_FIELD].split(';')
+    if len(splitted_note) <= 1:
+        showInfo("Nothing to split.")
+        return
+
+    card_english_type = browser.mw.col.models.by_name(TYPE_ENGLISH)
+    card_polish_read = browser.mw.col.models.by_name(READ_POLISH)
+
+    new_polish_note = browser.mw.col.new_note(card_polish_read)
+    for field in note.keys():
+        new_polish_note[field] = note[field]
+    new_polish_note[FOREIGN_FIELD] += f'[{len(splitted_note)}]'
+
+    deck_id = note.cards()[0].did
+    browser.mw.col.add_note(new_polish_note, deck_id)
+    move_queue_to_top(new_polish_note.id, browser)
+
+    for polish_word in splitted_note:
+        new_english_note = browser.mw.col.new_note(card_english_type)
+
+        for field in note.keys():
+            new_english_note[field] = note[field]
+        new_english_note[POLISH_FIELD] = polish_word
+
+        browser.mw.col.add_note(new_english_note, deck_id)
+        move_queue_to_top(new_english_note.id, browser)
+
+    delete_notes(selected, browser)
+    browser.model.reset()
+    showInfo(f"Splitted {', '.join(splitted_note)}")
+
 def add_custom_menu(browser: Browser):
-    # Action 1: Combine
+    # Action 1
     action_combine = QAction("Combine english synonymous", browser)
     action_combine.triggered.connect(lambda: combine_english_synonymous(browser))
 
-    # Action 2: Add single synonym
+    # Action 2
     action_add = QAction("Add english synonymous", browser)
     action_add.triggered.connect(lambda: add_english_synonym(browser))
+
+    # Action 3
+    action_split = QAction("Split polish synonymous", browser)
+    action_split.triggered.connect(lambda: split_polish_synonymous(browser))
 
     # Create menu and add both actions
     custom_menu = QMenu("MINE", browser)
     custom_menu.addAction(action_combine)
     custom_menu.addAction(action_add)
+    custom_menu.addAction(action_split)
 
     # Add the new custom menu to the browser's menu bar
     browser.form.menubar.addMenu(custom_menu)
